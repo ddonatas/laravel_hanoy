b1e355e999cee4ce3541607e82bb09f7ad7d350e
donatas <donatas@localhost>
2025-04-18 14:47:54 +0300

    master

---
+++ .history/routes/web_20250418111409.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+Route::get('/c', [ContactController::class, 'index'])->name('contacts.index');
+Route::get('/c/contacts/create', [ContactController::class, 'create'])->middleware('auth')->name('contacts.create');
+Route::post('/c/contacts', [ContactController::class, 'store'])->middleware('auth')->name('contacts.store');
